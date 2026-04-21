5cd47348f068d2bee7e63dba99c253cf9a10a825
totallynotzyk <basesukid@gmail.com>
2026-04-21 16:34:57 +0200

    Add comments to Disable CoreGui.lua for clarity
    Added comments to clarify the purpose of each UI element being disabled.

---
+++ NOT REQUIRE/Disable CoreGui.lua
@@ -0,0 +1,10 @@
+local StarterGui = game:GetService("StarterGui")
+
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, false) -- Emote Menu = Disabled
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false) -- Leaderboard UI = Disabled
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false) -- Chat UI = Disabled
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false) -- Inventory UI = Disabled
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Health, false) -- Health Bar UI = Disabled
+
+
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false) -- Everything UI = Disabled

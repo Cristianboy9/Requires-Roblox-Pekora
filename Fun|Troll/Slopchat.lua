1e70b2f522413e5b6f0048101f666486800d7c6f
Zyk <basesukid@gmail.com>
2026-04-29 16:41:17 +0200

    Create Slopchat.lua

---
+++ Fun|Troll/Slopchat.lua
@@ -0,0 +1,12 @@
+--[[
+]]
+-- // Slopchat, by #66xc6 (@Static) on disco
+
+-- // Silent (removes information when loaded so it isn't detected instantly)
+-- // Filter (lets you swear if false)
+
+-- // Lets all chat age groups talk to each other, no delay!
+
+require(101864411488724)(true, false) -- // Filter, Silent
+
+--]]
